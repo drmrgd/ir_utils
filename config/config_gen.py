6f176760ddd49f835deeffbd1592a6b93f280a20
Dave Sims <misc@lamneth.net>
2017-03-24 11:26:55 -0400

    Framed arg parsing and checking to make sure that we can get the right opts paired with the correct JSON file.

--- config/config_gen.py
+++ config/config_gen.py
@@ -1,66 +1,86 @@
 #!/usr/bin/python
 # Write a config script for package setup to create new config files for IR utils.  We'll need
 # to set up individual users' IP addresses, API Tokens, workflows, etc.  
-#
-# 
 ###############################################################################################
 import sys
 import os
 import json
 import argparse
 import datetime
-from distutils.version import LooseVersion
+from collections import defaultdict
 from pprint import pprint as pp
 
-
-version = '0.2.0_032117'
+version = '0.4.0_032117'
 
 def get_args():
     parser = argparse.ArgumentParser(
-        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 100, width=200),
+        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 100, width=150),
         description='''
-        <Program Description>
+        Configuration file generation utility for ir_utils.  Needed to make a IR API Retrieve and IR CLI Sample Creator
+        config files with credential and server information.  Can be used to generate a new, fresh config file, or to
+        update an existing config file.
         '''
     )
-    parser.add_argument('-n', '--new', choices=('api','sample'), help='Make a new config.json from template. Must choose type to make from list.')
-    parser.add_argument('-u', '--update', metavar='<JSON file>', help='Update key / value pair as a comma delimited list (e.g. key,"name of value to update".')
-    parser.add_argument('-s', '--server', metavar='hostname:IP_address', 
-            help='hostname and IP address, delimited by a colon, for new server to add to the api_retrieve config.')
-    parser.add_argument('-w', '--workflow', metavar='short_name:IR_workflow_name', 
-            help='short name and IR workflow name (quote names with spaces in them) to be added to the sample_creator config file.')
-    parser.add_argument('-j', '--json', metavar='<custom_json_filename>', 
-            help='Use custom JSON filename instead of defaults.  WARNING: This might break downstream scripts that rely on the JSON file to have a specific name!')
-    parser.add_argument('-v', '--version', action='version', version = '%(prog)s ' + version)
+    parser.add_argument('--new', choices=('api','sample'),
+            help='Make a new config.json from template. Must choose type to make from list.')
+
+    parser.add_argument('--update', metavar='<JSON file>', help='Update key / value pair as a comma delimited list (e.g. key,"name of value to update".')
+
+    parser.add_argument('--server', metavar='hostname:IP_address', 
+            help='Hostname and IP address, delimited by a colon, for new server to add to the api_retrieve config.')
+    parser.add_argument('--token', metavar='<api_token>', 
+            help='API Token used for IR API Retrieve.  Must be input for new IR connections.')
+
+    parser.add_argument('--workflow', metavar='short_name:IR_workflow_name', 
+            help='Short name and IR workflow name (quote names with spaces in them), delimited by a colon, to be added to the sample_creator config file.')
+
+    parser.add_argument('--version', action='version', version = '%(prog)s ' + version)
     args = parser.parse_args()
 
+    if not any((args.new, args.update)):
+        sys.stderr.write("ERROR: You must choose to either create a new JSON or update an existing one!\n\n")
+        print(parser.print_help())
+        sys.exit(1)
+    elif not args.workflow and not all((args.server,args.token)):
+        sys.stderr.write('ERROR: you must supply new data for either the api or sample creator config files!.\n\n')
+        print(parser.print_help())
+        sys.exit(1)
+    
+    # Choose which template we're working with.
     json_template = args.update
     if args.new == 'api':
         json_template = 'templates/ir_api_retrieve_config.json'
     elif args.new == 'sample':
         json_template = 'templates/ir_sample_creator_config.json'
 
-    new_data = {}
+    type_flag = os.path.basename(json_template).split('_')[1]
+    if type_flag == 'api' and args.workflow:
+        sys.stderr.write('ERROR: Mismatch between args (--workflow) and config type (--api)!\n\n')
+        print(parser.print_help())
+        sys.exit(1)
+    elif type_flag == 'sample' and args.server:
+        sys.stderr.write('ERROR: Mismatch between args (--server) and config type (--sample)!\n\n')
+        print(parser.print_help())
+        sys.exit(1)
+    
+    new_data = defaultdict(dict)
     if args.server:
-        new_data['hosts'] = parse_arg(args.server)
-    if args.workflow:
-        new_data['workflows'] = parse_arg(args.workflow)
+        if not args.token:
+            sys.stderr.write('ERROR: You must input a token for the new server using the --token opt')
+            print(parser.print_help())
+            sys.exit(1)
+        host,ip = args.server.split(':')
+        new_data['hosts'][host] = {
+            'ip' : ip,
+            'token' : args.token
+        }
+    elif args.workflow:
+        short_name,workflow = args.workflow.split(':')
+        new_data['workflows'][short_name] = workflow
 
-    if not args.workflow and not args.server:
-        sys.stderr.write('ERROR: you must supply new data for either the api or sample creator config files!.\n')
-        sys.exit(1)
-        
-    if not os.path.exists(json_template):
-        sys.stderr.write('ERROR: No such file or directory: %s\n' % json_template)
-        sys.exit(1)
+    pp(dict(new_data))
 
-    if args.json:
-        json_template = args.json
-
-    return json_template, new_data 
-
-def parse_arg(arg):
-    return dict(s.split(':') for s in arg.split(','))
-
+    return json_template, new_data, type_flag
 
 class Config(object):
     def __init__(self,config_file):
@@ -106,10 +126,10 @@ class Config(object):
         return data
 
 def main():
-    json_file,new_data = get_args()
+    json_file,new_data,config_type = get_args()
     
-    print(json_file)
-    print(new_data)
+    #print(json_file)
+    #print(new_data)
     
     config = Config(json_file)
     print(vars(config))
