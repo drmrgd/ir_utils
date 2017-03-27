adfd26e3bca487f097170facc61e4f621e441f2c
Dave Sims <misc@lamneth.net>
2017-03-27 13:37:37 -0400

    Added template retrieval and backup file methods. Added api config methods for new file and updated file.

--- config/config_gen.py
+++ config/config_gen.py
@@ -6,11 +6,13 @@ import sys
 import os
 import json
 import argparse
+import shutil
 import datetime
 from collections import defaultdict
+from termcolor import colored,cprint
 from pprint import pprint as pp
 
-version = '0.4.0_032117'
+version = '0.6.0_032117'
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -36,41 +38,51 @@ def get_args():
 
     parser.add_argument('--version', action='version', version = '%(prog)s ' + version)
     args = parser.parse_args()
+    print('args list:\n{}\n'.format(vars(args)))
+
+    # Determine method (new or update) and make sure we have the right args.
+    method = ''
+    if args.new:
+        method = 'new'
+    else:
+        method = 'update'
 
     if not any((args.new, args.update)):
-        sys.stderr.write("ERROR: You must choose to either create a new JSON or update an existing one!\n\n")
+        write_msg('err', "You must choose to either create a new JSON or update an existing one!\n\n")
         print(parser.print_help())
         sys.exit(1)
     elif not args.workflow and not all((args.server,args.token)):
-        sys.stderr.write('ERROR: you must supply new data for either the api or sample creator config files!.\n\n')
+        write_msg('err','Missing data! You must supply new data for either the api or sample creator config files!.\n\n')
         print(parser.print_help())
         sys.exit(1)
     
     # Choose which template we're working with.
     json_template = args.update
     if args.new == 'api':
-        json_template = 'templates/ir_api_retrieve_config.json'
+        json_template = 'templates/ir_api_retrieve_config.tmplt'
     elif args.new == 'sample':
-        json_template = 'templates/ir_sample_creator_config.json'
+        json_template = 'templates/ir_sample_creator_config.tmplt'
 
     type_flag = os.path.basename(json_template).split('_')[1]
     if type_flag == 'api' and args.workflow:
-        sys.stderr.write('ERROR: Mismatch between args (--workflow) and config type (--api)!\n\n')
+        write_msg('err','Mismatch between args (--workflow) and config type (--api)!\n\n')
         print(parser.print_help())
         sys.exit(1)
     elif type_flag == 'sample' and args.server:
-        sys.stderr.write('ERROR: Mismatch between args (--server) and config type (--sample)!\n\n')
+        write_msg('err','Mismatch between args (--server) and config type (--sample)!\n\n')
         print(parser.print_help())
         sys.exit(1)
     
+    # Set up dict of new data to process
     new_data = defaultdict(dict)
     if args.server:
         if not args.token:
-            sys.stderr.write('ERROR: You must input a token for the new server using the --token opt')
+            write_msg('err','You must input a token for the new server using the --token opt')
             print(parser.print_help())
             sys.exit(1)
         host,ip = args.server.split(':')
-        new_data['hosts'][host] = {
+        # new_data['hosts'][host] = {
+        new_data[host] = {
             'ip' : ip,
             'token' : args.token
         }
@@ -78,9 +90,12 @@ def get_args():
         short_name,workflow = args.workflow.split(':')
         new_data['workflows'][short_name] = workflow
 
+    print("new data: ")
     pp(dict(new_data))
+    print('')
 
-    return json_template, new_data, type_flag
+    # TODO:  Should probably output new or update so that we can do some file backing up adn stuff.
+    return method, json_template, new_data, type_flag
 
 class Config(object):
     def __init__(self,config_file):
@@ -92,7 +107,7 @@ class Config(object):
         return '{}:{}'.format(self.__class__,self.__dict__)
 
     def __str__(self):
-        return pp(dict(self.config_data))
+        return str(pp(self.config_data))
 
     def __getitem__(self,key):
         return self.config_data[key]
@@ -109,14 +124,29 @@ class Config(object):
     def add_workflow(self,workflow_id,workflow_name):
         return self.config_data['workflows'].update({workflow_id: workflow_name})
 
-    def add_host(self,host,ip):
-        return self.config_data['hosts'].update({host : ip})
+    def add_host(self,data):
+        '''Add host, ip, and token to config file.  Requires a dict of host data in the form:
+                {<hostname> : { 'ip' : <ip_address>, 'token' : <api_token>}}
+           Since we're using a dict.update() method, can also use same function for updating the 
+           config file.
+        '''
+        return self.config_data['hosts'].update(data)
 
     def write_config(self,filename=None):
-        with open(filename, 'w') as out_fh:
+        '''Write the config file out to disk. Have left room for custom naming, though this is not
+        preferred as the utils are specifically looking for a certain filename.
+        '''
+        if filename:
+            json_out = filename
+        else: 
+            json_out = self.config_file
+        with open(json_out, 'w') as out_fh:
             json.dump(self.config_data,out_fh,indent=4,sort_keys=False)
 
-    def make_blank_template(self,method):
+    def __make_blank_template(self,method):
+        '''Make a brand new shiny template file to use for something else. Not yet needed, but can
+        add later.
+        '''
         return
     
     @classmethod
@@ -125,15 +155,46 @@ class Config(object):
             data = json.load(fh)
         return data
 
-def main():
-    json_file,new_data,config_type = get_args()
-    
-    #print(json_file)
-    #print(new_data)
-    
+def write_msg(flag, string):
+    if flag == 'err':
+        cprint("ERROR: ", 'red', attrs=['bold'], end='', file=sys.stderr)
+    elif flag == 'warn':
+        cprint('WARN: ', 'yellow', attrs=['bold'], end='', file=sys.stderr)
+    elif flag == 'info':
+        cprint('INFO: ', 'cyan', attrs=['bold'], end='', file=sys.stderr)
+    print(string)
+    return
+
+def edit_config(json_file,config_type,new_data):
     config = Config(json_file)
-    print(vars(config))
+    print('config obj:\n{}\n'.format(vars(config)))
+    if config_type == 'api':
+        config.add_host(new_data)
+        config.write_config()
+        # config.old_add_host('test','123.456.789.900')
+        print(config)
+    return
+
+def backup_config(jfile):
+    shutil.copy(jfile,jfile+'~')
+
+def main():
+    method,source_json_file,new_data,config_type = get_args()
+
+    # If we're making a new config file, copy a blank from templates and edit into this dir.  
+    # If we just want to update the current config file, then make a backup and then edit.
+    if method == 'new':
+        new_json = os.path.join(os.getcwd(),os.path.basename(source_json_file).replace('tmplt','json'))
+        if os.path.exists(new_json):
+            backup_config(new_json)
+        shutil.copy(source_json_file,new_json)
+        edit_config(new_json,config_type,new_data)
+    else:
+        '''backup the current config file and edit'''
+        backup_config(source_json_file)
+        edit_config(source_json_file,config_type,new_data)
     sys.exit()
+
     config.add_host('nci','129.43.127.244')
     config.add_host('drt','129.43.127.192')
     config.add_host('foo','some.invalid.ip')
