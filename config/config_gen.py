0a56ef96552da03932fcb8628a19dd2c979e389d
Dave Sims <misc@lamneth.net>
2017-05-17 10:14:37 -0400

    Refactored much of script to accomodate new paired and single workflows, plus to fix some logic bugs

--- config/config_gen.py
+++ config/config_gen.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 from termcolor import colored,cprint
 from pprint import pprint as pp
 
-version = '1.0.0_032117'
+version = '1.3.0_051717'
 
 
 class Config(object):
@@ -41,7 +41,7 @@ class Config(object):
 
     def add_workflow(self,data):
         '''Add workflow shortname and IR matching name to config file.  Requires a dict of workflow data in the form:
-                {<short_name> : <ir_name>}
+                {<single|paired> : {<short_name> : <ir_name>}}
            Since we're using a dict.update() method, can also use same function for updating the 
            config file.
         '''
@@ -81,7 +81,8 @@ class Config(object):
 
 def get_args():
     parser = argparse.ArgumentParser(
-        formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 100, width=150),
+        # formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 100, width=125),
+        formatter_class = lambda prog: argparse.HelpFormatter(prog, width=125),
         description='''
         Configuration file generation utility for ir_utils.  Needed to make a IR API Retrieve and IR CLI Sample Creator
         config files with credential and server information.  Can be used to generate a new, fresh config file, or to
@@ -89,67 +90,68 @@ def get_args():
         of the last config file if one existed.
         '''
     )
-    parser.add_argument('--new', choices=('api','sample'),
-            help='Make a new config.json from template. Must choose type to make from list.')
-    parser.add_argument('--update', metavar='<JSON file>', help='Update key / value pair as a comma delimited list (e.g. key,"name of value to update".')
+    parser.add_argument('-m', '--method', choices=('api','sample'), required=True,
+            help='Type of config file to be made or updated.')
+    parser.add_argument('-u', '--update', metavar='<JSON file>', 
+            help='''Update a config file with new data. Must use either the "server" and "token" options for an 
+                    API config, or the 'workflow' and 'analysis_type' options for a sample uploader config''')
     
-    parser.add_argument('--server', metavar='hostname:IP_address', 
+    parser.add_argument('-s', '--server', metavar='<hostname:IP_address>', 
             help='Hostname and IP address, delimited by a colon, for new server to add to the api_retrieve config.')
-    parser.add_argument('--token', metavar='<api_token>', 
-            help='API Token used for IR API Retrieve.  Must be input for new IR connections.')
+    parser.add_argument('-t', '--token', metavar='<api_token>', 
+            help='API Token used for IR API Retrieve. This can be determined from a user account on the IR server to be added..')
 
-    parser.add_argument('--workflow', metavar='short_name:IR_workflow_name', 
-            help='Short name and IR workflow name (quote names with spaces in them), delimited by a colon, to be added to the sample_creator config file.')
-    parser.add_argument('-type', choice=['single','paired'],
-            help='Used with workflow config file. Indicate if the workflow is for a paired DNA / RNA or a single RNA / single DNA specimen.')
+    parser.add_argument('-w', '--workflow', metavar='short_name:IR_workflow_name', 
+            help='''Short name and IR workflow name (quote names with spaces in them), delimited by a colon, to be added 
+                    to the sample_creator config file. The full workflow name must match exactly the name that's indicated
+                    in the IR GUI.''')
+    parser.add_argument('-a', '--analysis_type', choices=('single','paired'),
+            help='Indicate if the workflow is for a paired DNA / RNA or a single RNA / single DNA specimen.')
 
     # TODO: Add a read from flat file option?
-    parser.add_argument('--file', metavar='<file>', 
+    parser.add_argument('-f', '--file', metavar='<file>', 
             help='Read config data from a flat CSV file rather than inputting each element on the commandline.  Helpful for instances where we need to add a lot of stuff to one config.')
 
     parser.add_argument('--version', action='version', version = '%(prog)s ' + version)
     args = parser.parse_args()
 
-    # Determine method (new or update) and make sure we have the right args.
-    method = ''
-    if args.new:
-        method = 'new'
+    # Get and check the passed args.
+    if args.file:
+        '''process the file accordingly'''
+        print('Getting params from a flat file: %s' % (args.file))
+        print('this is not yet implemented, so skipping!')
+        sys.exit(213)
     else:
-        method = 'update'
+        if args.method == 'sample' and not all((args.workflow,args.analysis_type)):
+                write_msg('err','Missing data! You must indicate the workflow name and if the new workflow is a "paired" or "single" workflow when using the "sample" method!\n\n')
+                parser.print_help()
+                sys.exit(1)
+        elif args.method == 'api' and not all((args.server,args.token)):
+                write_msg('err','Missing data! You must indicate the server name and input an API token when using the API method.\n\n')
+                parser.print_help()
+                sys.exit(1)
 
-    if not any((args.new, args.update)):
-        write_msg('err', "You must choose to either create a new JSON or update an existing one!\n\n")
-        print(parser.print_help())
-        sys.exit(1)
-    elif not args.workflow and not all((args.server,args.token)):
-        write_msg('err','Missing data! You must supply new data for either the api or sample creator config files!.\n\n')
-        print(parser.print_help())
-        sys.exit(1)
-    
-    # Choose which template we're working with.
-    json_template = args.update
-    if args.new == 'api':
+    # Choose which template we're working with and check to be sure it's the right one
+    if args.update:
+        json_template = args.update
+    elif args.method == 'api':
         json_template = 'templates/ir_api_retrieve_config.tmplt'
-    elif args.new == 'sample':
+    elif args.method == 'sample':
         json_template = 'templates/ir_sample_creator_config.tmplt'
 
     type_flag = os.path.basename(json_template).split('_')[1]
     if type_flag == 'api' and args.workflow:
         write_msg('err','Mismatch between args (--workflow) and config type (--api)!\n\n')
-        print(parser.print_help())
+        parser.print_help()
         sys.exit(1)
     elif type_flag == 'sample' and args.server:
         write_msg('err','Mismatch between args (--server) and config type (--sample)!\n\n')
-        print(parser.print_help())
+        parser.print_help()
         sys.exit(1)
     
     # Set up dict of new data to process
     new_data = defaultdict(dict)
-    if args.server:
-        if not args.token:
-            write_msg('err','You must input a token for the new server using the --token opt')
-            print(parser.print_help())
-            sys.exit(1)
+    if args.method == 'api':
         host,ip = args.server.split(':')
         if not ip.startswith('https://'):
             ip = 'https://' + ip
@@ -157,11 +159,10 @@ def get_args():
             'ip' : ip,
             'token' : args.token
         }
-    elif args.workflow:
+    elif args.method == 'sample':
         short_name,workflow = args.workflow.split(':')
-        new_data[short_name] = workflow
-
-    return method, json_template, new_data, type_flag
+        new_data[args.analysis_type][short_name] = workflow
+    return args.method, json_template, new_data, args.update
 
 
 def write_msg(flag, string):
@@ -187,18 +188,26 @@ def backup_config(jfile):
     shutil.copy(jfile,jfile+'~')
 
 def main():
-    method,source_json_file,new_data,config_type = get_args()
+    method,source_json_file,new_data,update = get_args()
+    debug = True
+    if debug:
+        print('{}  DEBUG  {}'.format('-'*30, '-'*30))
+        print('method: {}\nsource_json_file: {}\nupdate: {}'.format(method, source_json_file, update))
+        pp(dict(new_data))
+        print('-'*69)
 
-    if method == 'new':
+    if update:
+        '''backup the current config file and edit'''
+        print('Updating {}.'.format(source_json_file))
+        backup_config(source_json_file)
+        edit_config(source_json_file,method,new_data)
+    else: 
         new_json = os.path.join(os.getcwd(),os.path.basename(source_json_file).replace('tmplt','json'))
+        print('Making a new JSON file: {}'.format(new_json))
         if os.path.exists(new_json):
             backup_config(new_json)
         shutil.copy(source_json_file,new_json)
-        edit_config(new_json,config_type,new_data)
-    else:
-        '''backup the current config file and edit'''
-        backup_config(source_json_file)
-        edit_config(source_json_file,config_type,new_data)
+        edit_config(new_json,method,new_data)
 
 if __name__ == '__main__':
     main()
